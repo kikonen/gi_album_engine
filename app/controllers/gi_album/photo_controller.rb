35e7398847347d2b6c5c467bdeeab021c6190a0b
kari <mr.kari.ikonen@gmail.com>
2016-07-09 10:27:28 +0300

    EXtra debug info and new dev photo album

--- app/controllers/gi_album/photo_controller.rb
+++ app/controllers/gi_album/photo_controller.rb
@@ -2,6 +2,8 @@ module GiAlbum
   class PhotoController < ::ApiController
     def index
       album_path = URI.unescape(params[:dir])
+      logger.debug "PATH: #{album_path}"
+
       elements = album.list(album_path)
 
       render :json, partial: "gi_album/photo/index.json", locals: { elements: elements }
